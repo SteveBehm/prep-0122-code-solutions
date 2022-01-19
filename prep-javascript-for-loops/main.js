ac66ff736e39c1539a066021e062b7a60358b851
Steve Behm <stevtbehm1@gmail.com>
2022-01-19 12:54:03 -0800

    completed prep-javascript-for-loops exercise.

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,13 @@
+// create your loops here.
+var myArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+for (let i = 0; i < myArray.length - 1; i++) {
+  console.log(myArray[i]);
+}
+
+for (let i = 0; i < 20; i += 2) {
+  console.log(i);
+}
+
+for (let i = 100; i >= 0; i--) {
+  console.log('Time till explosion ', i);
+}
